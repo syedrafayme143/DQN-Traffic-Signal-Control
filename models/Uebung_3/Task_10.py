863e60f84f18f8c4c330da27906993df9f7b5652
Syed Rafay Ali <syedrafayme143@gmail.com>
2026-02-12 23:12:14 +0100

    Adding models

---
+++ models/Uebung_3/Task_10.py
@@ -0,0 +1,8 @@
+"""
+Qualitative Comparison (Task 10):
+
+The volume_lane_fast representation (1st graph) shows faster convergence with smoother reward improvement 
+and fewer deep reward drops, indicating more stable and efficient learning. In contrast, the congestion_length 
+representation (2nd graph) exhibits more volatility and slower convergence, suggesting it may provide less 
+informative or noisier input for the agent’s decision-making process.
+"""
